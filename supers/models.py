cbc99806ccb4d92b016bb4dfd06e9e3f7ec5e2a0
Fuller1212 <fullermorrison@gmail.com>
2022-07-15 13:51:20 -0500

    Created SuperType Class + Super Class in each App Model

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+class Super(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    second_ability = models.CharField(max_length=255)
