231f9c4fd23c1b49cb824555fe02e3800878de8c
Fuller1212 <fullermorrison@gmail.com>
2022-07-18 15:03:30 -0500

    Fixed error in Code + Migrated New Objects

--- supers/serializers.py
+++ supers/serializers.py
@@ -5,6 +5,6 @@ from supers.models import Super
 class SuperSerializer(serializers.ModelSerializer):
     class Meta:
         model = Super
-        fields = ['name', 'alter_ego', 'primary_ability', 'second_ability', 'catchphrase', 'super_style', 'super_style_id']
+        fields = ['name', 'alter_ego', 'primary_ability', 'second_ability', 'catchphrase', 'super_type', 'super_type_id']
         depth = 1
     super_style_id = serializers.IntegerField(write_only = True)
